387e972287e934346f568f9f9f649f858a4a1f34
dazacher <dazacher2000@yahoo.com>
2021-07-16 07:03:23 -0700

    New Integer Type.

--- src/generic_io.adb
+++ src/generic_io.adb
@@ -1,6 +1,12 @@
-procedure Generic_Io is
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
+procedure Generic_Io is
+    Int_Sub : Positive := 1;
+    type My_Int is new Integer;
+    package New_IO is new Ada.Text_IO.Integer_IO(My_Int);
+    Test_New : My_Int := 1;
 begin
-   --  Insert code here.
-   null;
+    Put(Int_Sub);
+    New_IO.Put(Test_New);
 end Generic_Io;
